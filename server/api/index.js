2b5959b4a5c623688bb0edf2422bc93fc9cbac1b
Fahad <78952725+Ranger-NF@users.noreply.github.com>
2025-07-26 12:00:03 +0530

    Added cors

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const fs = require("fs");
+const cors = require("cors");
 const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
+
+app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
